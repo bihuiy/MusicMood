65fb70f67f3694f053ef9e7c9feef9975af5f84d
Vivien <bihuiy7@gmail.com>
2025-07-25 22:31:14 +1000

    fix comment styling error

--- middleware/isSignedIn.js
+++ middleware/isSignedIn.js
@@ -2,7 +2,7 @@ const isSignedIn = (req, res, next) => {
   if (req.session.user) return next();
   else {
     req.session.message = "Please sign in to continue.";
-    req.session.redirectTo = req.originalUrl;
+    //req.session.redirectTo = req.originalUrl;
     return res.redirect("/");
   }
 };
